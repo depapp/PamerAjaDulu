573f3fa5b18a3f3bdd7b0a11f4d4f820bedfc325
depapp <depa.article@gmail.com>
2024-05-03 09:20:38 +0700

    fix No Stylesheets In Head Component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,12 +7,6 @@ const Layout = (props) => {
     <>
       <Head>
         <title>#PamerAjaDulu</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-          crossOrigin="anonymous"
-        />
       </Head>
       <Header />
       <main>{props.children}</main>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
